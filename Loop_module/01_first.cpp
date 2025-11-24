3ca673353eccdf5218a2ba91a0c6347d80d176c0
sandeepkumar-demo17 <kudupudisandeepkumar88@gmail.com>
2025-11-24 23:12:05 +0530

    first commit

---
+++ Loop_module/01_first.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,table;
+    cout<<"ENTER NUMBER :";
+    cin>>n;
+    for(int i=1;i<=10;i++)
+    {
+        cout<<n<<"x"<<i<<"="<<n*i<<endl;
+    }
+}
