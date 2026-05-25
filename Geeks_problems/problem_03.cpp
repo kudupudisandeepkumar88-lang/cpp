7e7276e1208736e6a9b7f81ab7fdd7b038a69159
sandeepkumar-demo17 <kudupudisandeepkumar88@gmail.com>
2026-05-25 08:04:48 +0530

    Solved solid square pattern

---
+++ Geeks_problems/problem_03.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int p,q,r,s,t;
+    int x=3,y=4;
+    p=x+y,q=x-y,r=x*y,s=x/y,t=x%y;
+    cout << p << " " << q << " " << r << " " << s << " " << t << endl;
+    return 0;
+}
