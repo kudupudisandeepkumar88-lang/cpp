1cb1890010c26ef61eaff31d615b243eef93eecb
sandeepkumar-demo17 <kudupudisandeepkumar88@gmail.com>
2026-05-26 15:41:56 +0530

    Solved problem 06

--- arrays_module/05_first.cpp
+++ arrays_module/05_first.cpp
@@ -1,13 +1,10 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int i;
-    int arr[5]={3,9,8,7,6};
-    int max=arr[0];
-    for(int i=0;i<6;i++){
-        if(arr[i]>max){
-            max=arr[i];
+    for(int i=1;i<=4;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"* ";
         }
+        cout<<endl;
     }
-    cout<<max;
 }
