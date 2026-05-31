ec68e4743f6f07554da162b262163ecdd74cee9c
sandeepkumar-demo17 <kudupudisandeepkumar88@gmail.com>
2026-05-31 22:30:49 +0530

    solved basic problem

---
+++ Geeks_problems/problem_09.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    void fizzBuzz(int number) {
+        if(number%3==0 && number%5==0){
+            cout<<"FizzBuzz\n";
+        }    
+        else if(number%3==0){
+            cout<<"Fizz\n";
+        }
+        else if(number%5==0){
+            cout<<"Buzz\n";
+        
+        }
+        else{
+            cout<<number<<"\n";
+        }
+    }
+};
